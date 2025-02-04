34dd40c2575db63eb305cbfdbcddf8bcfc23da34
Diogo Krüger <diogoks2005@gmail.com>
2025-02-04 01:50:45 -0300

    Agora com o padrão de código de  Doom3

--- agendaCERTA.c
+++ agendaCERTA.c
@@ -3,7 +3,14 @@
 #include <string.h>
 
 #define TAM_MAX 31
-
+/*
+====================
+ArrumarOsPonteiros
+  Pega todos os ponteiros usados no código
+  e faz eles serem apontados para os seus
+  devidos lugares na memória alocada.
+====================
+*/
 void ArrumarOsPonteiros(void **buffer, int **i, int **op, int **numChars, int **numTempChars, int **tamanhoStringTemp, char **tempNome, char **stringTemp2){
     *i                   = *buffer;
     *op                  = *buffer + sizeof(int);
@@ -15,19 +22,35 @@ void ArrumarOsPonteiros(void **buffer, int **i, int **op, int **numChars, int **
     return;
 }
 
+/*
+====================
+Adicionar
+  Adiciona uma pessoa, juntamente com
+  as suas informações, como idade e
+  email, à agenda.
+====================
+*/
 void Adicionar(void **buffer, int *i, int *op, int *numChars, int *numTempChars, int *tamanhoStringTemp, char *tempNome, char *nome, char *stringTemp2){
-
+    *numTempChars = 0;
+    *op = 0;
+    nome = *buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2);
     for(*i = 0; *i < 3; (*i)++){
         
         switch(*i){
             case 0:
                 printf("Digite o nome: (máx 30 caract.): ");
                 gets(tempNome);
-                    if(strstr(*buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2), tempNome)){
+                //   op sendo usado como i
+                //   op = ints iniciais     +  chars iniciais             |  op < ints iniciais           + chars iniciais               +  numero de chars           |  op = próxima string
+                for( *op = (sizeof(int) * 5 ) + (sizeof(char) * TAM_MAX * 2); *op < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); *op = *op + *numTempChars){
+                    if( strcmp(nome, tempNome) == 0 ){
                         printf("Não dá pra adicionar essa pessoa, ela já está na agenda...\n");
-                        return; 
-                    } 
-                break;
+                        return; //ESSA PESSOA JÁ EXISTE, SAI DA FUNÇÃO
+                    }
+                    *numTempChars += strlen(nome) + 1; // numTempChars SOMA-SE COM TAMANHO DO NOME
+                    nome += (sizeof(char) * (strlen(nome) + 1)); // NOME PASSA PRO PRÓX. NOME
+                }  //PROCURA PELA MEMORIA ATÉ ACHAR A PESSOA, SE ACHAR, SAI                                    
+                break; //SE CHEGAR AQUI, O NOME NÃO EXISTE NA MEMÓRIA   
         
             case 1:
                 printf("Digite a idade: (máx 30 caract.): ");
@@ -52,8 +75,17 @@ void Adicionar(void **buffer, int *i, int *op, int *numChars, int *numTempChars,
     printf("Adicionado!\n");
 }
 
+/*
+====================
+RemoverPessoa
+  Remove uma pessoa da agenda
+  junto com todas as suas informações,
+  movendo toda a memória na frente dessa
+  pessoa para onde essa pessoa ficava.
+====================
+*/
 void RemoverPessoa(void **buffer, int *i, int *op, int *numChars, int *numTempChars, int *tamanhoStringTemp, char *tempNome, char *nome, char *stringTemp2){
-    if(*numChars == 0){
+    if( *numChars == 0 ){
         printf("A agenda está vazia, impossível remover alguém.\n");
         return;
     }
@@ -63,59 +95,74 @@ void RemoverPessoa(void **buffer, int *i, int *op, int *numChars, int *numTempCh
     *numTempChars = 0;
     *op = 0;
 
-    if(strstr(*buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2), tempNome)){
-        printf("%s está na agenda e será removido junto com suas informações!\n", tempNome);
+nome = *buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2);
 
-        nome = strstr(*buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2), tempNome);
+//   i =  ints iniciais       chars iniciais             |  i < tamanho total da memoria                                                            |  i passa pra próxima string
+for(*i = (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2); *i < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); *i = *i + *numTempChars){
+    if( strcmp(nome, tempNome) == 0 ){
+        printf("%s está na agenda e será removido junto com suas informações!\n", tempNome); //ACHOU!!!
 
-        for(*i = 0; *i <3; (*i)++){
-            *numTempChars += strlen(nome) + 1;
-            nome += (sizeof(char) * (strlen(nome) + 1));
-        }
-        
-        for(*i = (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2); *i < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); *i = *i + *tamanhoStringTemp){
-        //       ints iniciais      nome temporario              |  enquanto o i < ints iniciais, o nome temporario, o num de chars                     | i pula pro proximo blocão de chars
-            if(strcmp(nome, stringTemp2) == 0){
-                break;
-            }
+        *tamanhoStringTemp = *numTempChars;
+        *numTempChars = 0;
+        for( *op = 0; (*op < 3); (*op)++ ){
+            *numTempChars += strlen(nome) + 1; // numTempChars SOMA-SE COM O TAMANHO DO NOME
+            nome += (sizeof(char) * (strlen(nome) + 1)); // PASSA PRO PRÓXIMO NOME
+        } //numTempChars COMEÇA A TER O TAMANHO A SER TIRADO
 
-            *tamanhoStringTemp += strlen(stringTemp2) + 1;
-            stringTemp2 += (sizeof(char)) * (strlen(stringTemp2) + 1);
-        }
-        
-        for(*i = (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) * (sizeof(char) * (int)*tamanhoStringTemp); *i < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); *i = *i + *op){
-            *op += strlen(nome) + 1;
-            nome += (sizeof(char) * (strlen(nome) + 1));
-        }
+            stringTemp2 = tempNome; //stringTemp2 vira o nome, para calcular o tamanho a ser movido
 
-        tempNome = strstr(*buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2), tempNome);
-        memmove(tempNome, nome, sizeof(char) * *op);
+            //    op = ints iniciais        chars iniciais                 tempNome                           |  op < ints iniciais             chars iniciais                  quantidade de chars total |    op pula pra proxima string
+            for( *op = (sizeof(int) * 5 ) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *tamanhoStringTemp); *op < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); (*op) = *op + *tamanhoStringTemp){
+                *tamanhoStringTemp += strlen(stringTemp2) + 1; //tamanhoStrinTemp2 se soma com o tamanho da string que ele está
+                stringTemp2 += sizeof(char) * strlen(stringTemp2) + 1; //stringTemp2 passa pra próxima string
+            } 
+            *tamanhoStringTemp = *tamanhoStringTemp - *numTempChars;
+            // tamanhoStringTemp COMEÇA A TER O TAMANHO A SER MOVIDO
 
+            memmove(tempNome, nome, sizeof(char) * *tamanhoStringTemp);
+
+        //                        pBuffer   ints iniciais    +  chars iniciais              + numChars - numTempChars               
         *buffer = (void*) realloc(*buffer, (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + ((sizeof(char) * *numChars) - (sizeof(char) * *numTempChars)));
-        
+
         ArrumarOsPonteiros(buffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2); 
         *numChars = *numChars - *numTempChars;
 
         printf("Removido!\n");
+        return; //ESSA PESSOA FOI REMOVIDA
     }
-    else{
-        printf("%s não está na agenda, não dá para removê-lo...\n", tempNome);
-    }
+    //NOME FICA PULANDO PRO PRÓXIMO ATÉ ACHAR A PESSOA
+    *numTempChars += strlen(nome) + 1; // numTempChars SOMA-SE COM TAMANHO DO NOME
+    nome += (sizeof(char) * (strlen(nome) + 1)); // NOME PASSA PRO PRÓX. NOME
+
+}  //PROCURA PELA MEMORIA ATÉ ACHAR A PESSOA                                
+
+printf("%s não está na agenda...\n", tempNome);
 }
 
-void BuscaPessoa(void **buffer, int *numChars, int *numTempChars, char *tempNome, char *nome){
-    if(*numChars == 0){
+/*
+====================
+BuscaPessoa
+  Baseado na escolha do usuário,
+  busca uma pessoa, mostrando se
+  ela está ou não na agenda, caso 
+  estiver, mostra também suas informações.
+====================
+*/
+void BuscaPessoa(void **buffer, int *i, int *numChars, int *numTempChars, char *tempNome, char *nome){
+    if( *numChars == 0 ){
         printf("A agenda está vazia, não há como buscar ninguém.\n");
         return;
     }
+
     printf("Quem desejas buscar: (buscar pelo nome) (máx 30 caract.): ");
     gets(tempNome);
     *numTempChars = 0;
+    nome = *buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2);
 
-    if(strstr(*buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2), tempNome)){
-        printf("%s está na agenda!\n", tempNome);
+for(*i = (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2); *i < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); *i = *i + *numTempChars){
+    if( strcmp(nome, tempNome) == 0 ){
+        printf("%s está na agenda!\n", tempNome); //ACHOU!!!
 
-        nome = strstr(*buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2), tempNome);
         *numTempChars += strlen(nome) + 1;
         nome += (sizeof(char) * (strlen(nome) + 1));
         printf("Suas outras informações são: \n");
@@ -125,14 +172,27 @@ void BuscaPessoa(void **buffer, int *numChars, int *numTempChars, char *tempNome
         nome += (sizeof(char) * (strlen(nome) + 1));
 
         printf("\tEmail: %s\n", nome);
+        return; //ESSA PESSOA EXISTE
     }
-    else{
-        printf("%s não está na agenda...\n", tempNome);
-    }
+
+    *numTempChars += strlen(nome) + 1; // numTempChars SOMA-SE COM TAMANHO DO NOME
+    nome += (sizeof(char) * (strlen(nome) + 1)); // NOME PASSA PRO PRÓX. NOME
+
+}  //PROCURA PELA MEMORIA ATÉ ACHAR A PESSOA                                
+
+printf("%s não está na agenda...\n", tempNome);       
 }
 
+/*
+====================
+ListaTodos
+  Lista todas as pessoas presentes
+  até o momento na agenda, junto com
+  todas as suas informações.
+====================
+*/
 void ListaTodos(void **buffer, int *i, int *op, int *numChars, int *numTempChars, char *nome){
-    if(*numChars == 0){
+    if( *numChars == 0 ){
         printf("A agenda está vazia, não há ninguém para mostrar.\n");
         return;
     }
@@ -142,7 +202,9 @@ void ListaTodos(void **buffer, int *i, int *op, int *numChars, int *numTempChars
     nome = *buffer + (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2);
 
     printf("Todas as pessoas na agenda até agora:\n");
-    for(*i = (sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2); *i < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); *i = *i + *numTempChars){
+
+    //   i = ints iniciais     +  chars iniciais             |  i < ints iniciais           + chars iniciais               +  numero de chars           |  i = próxima string
+    for( *i = (sizeof(int) * 5 ) + (sizeof(char) * TAM_MAX * 2); *i < (int)((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2) + (sizeof(char) * *numChars)); *i = *i + *numTempChars){
         (*op)++; 
         printf("Pessoa %d: \n", *op);
 
@@ -160,44 +222,49 @@ void ListaTodos(void **buffer, int *i, int *op, int *numChars, int *numTempChars
     }
 }
 
+/*
+====================
+main
+  Onde ocorre a alocação da memória principal,
+  o usuário escolhe as operações que ele deseja
+  fazer em questão à agenda, e onde a memória é liberada. 
+====================
+*/
 int main() {
-    void *pBuffer = (void*) malloc((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2));
-    
-    int *i, *op, *numChars, *numTempChars, *tamanhoStringTemp;           
-    char *tempNome, *nome, *stringTemp2;
-    
-    ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
-    nome = NULL;
+  void *pBuffer = (void*) malloc((sizeof(int) * 5) + (sizeof(char) * TAM_MAX * 2));
+  
+  int *i, *op, *numChars, *numTempChars, *tamanhoStringTemp;           
+  char *tempNome, *nome, *stringTemp2;
+  
+  ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
+  nome = NULL;
+  *numChars = 0;
+  *numTempChars = 0;
+  
+  do{
+      printf("--- AGENDA --------------------------------\n1 - Adicionar Pessoa (Nome, Idade e E-Mail)\n2 - Remover Pessoa\n3 - Buscar Pessoa\n4 - Listar Todos\n5 - Sair\n-------------------------------------------\n=>");
+      scanf("%d", op);
+      getchar();
+          switch(*op){
+              case 1: Adicionar(&pBuffer, i, op, numChars, numTempChars, tamanhoStringTemp, tempNome, nome, stringTemp2);
+                      ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2); 
+                      break;
+              case 2: RemoverPessoa(&pBuffer, i, op, numChars, numTempChars, tamanhoStringTemp, tempNome, nome, stringTemp2);
+                      ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
+                      break;
+              case 3: BuscaPessoa(&pBuffer, i, numChars, numTempChars, tempNome, nome);
+                      ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
+                      break;
+              case 4: ListaTodos(&pBuffer, i, op, numChars, numTempChars, nome);
+                      ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
+                      break;
+              case 5: printf("Ok, tchau!\n"); 
+                      break;
+              default: printf("Número inválido! Tente um número de 1-5.\n");
+          }
+  }while( *op != 5 );
 
-    *numChars = 0;
-    *numTempChars = 0;
-    
-    do{
-        printf("--- AGENDA --------------------------------\n1 - Adicionar Pessoa (Nome, Idade e E-Mail)\n2 - Remover Pessoa\n3 - Buscar Pessoa\n4 - Listar Todos\n5 - Sair\n-------------------------------------------\n=>");
-        scanf("%d", op);
-        getchar();
-            switch(*op){
-                case 1: Adicionar(&pBuffer, i, op, numChars, numTempChars, tamanhoStringTemp, tempNome, nome, stringTemp2);
-                        ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2); 
-                        break;
-                case 2: RemoverPessoa(&pBuffer, i, op, numChars, numTempChars, tamanhoStringTemp, tempNome, nome, stringTemp2);
-                        ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
-                        break;
-                case 3: BuscaPessoa(&pBuffer, numChars, numTempChars, tempNome, nome);
-                        ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
-                        break;
-                case 4: ListaTodos(&pBuffer, i, op, numChars, numTempChars, nome);
-                        ArrumarOsPonteiros(&pBuffer, &i, &op, &numChars, &numTempChars, &tamanhoStringTemp, &tempNome, &stringTemp2);
-                        break;
-                case 5: printf("Ok, tchau!\n"); 
-                        break;
-                default: printf("Número inválido! Tente um número de 1-5.\n");
-            }
-    }while(*op != 5);
+  free(pBuffer);
 
-    printf("%d", *numChars);
-    
-    free(pBuffer);
-
-    return 0;
+  return 0;
 }
